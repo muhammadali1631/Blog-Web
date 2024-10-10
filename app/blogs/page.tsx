4b390dec59a7172761da8a5e0229cebcce241218
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-10 23:03:48 +0500

    solving error round 2

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -11,7 +11,7 @@ const LatestWebDevlopment = () => {
        <h1 className='text-4xl text-center my-10 font-semibold'>All Blogs</h1>
        <div className='flex flex-wrap w-[100%] justify-around gap-10'>
                 {data.map((item) => (
-                    <div className='bg-[#09003d] text-[#fff] p-8 gap-5 rounded-xl w-[350px] grid justify-between box-shad text-center sm:w-[500px] cursor-pointer mb-7 scale-in-bottom1 hover:bg-[#090111] hover:duration-700 hover:ease-in-out'>
+                    <div key={item.title} className='bg-[#09003d] text-[#fff] p-8 gap-5 rounded-xl w-[350px] grid justify-between box-shad text-center sm:w-[500px] cursor-pointer mb-7 scale-in-bottom1 hover:bg-[#090111] hover:duration-700 hover:ease-in-out'>
                         <Image src={item.image} alt={item.title} className='w-full h-44 object-cover rounded-md' width={0} height={0}/>
                         <h2 className='text-2xl font-bold'>{item.title}</h2>
                         <p className='text-md'>{typeof item.description === 'string' ? item.description : item.description.content}</p>
--- components/blogsider.tsx
+++ components/blogsider.tsx
@@ -10,17 +10,16 @@ const BlogSider = (props:Blogs) => {
   return (
     <div className='bg-[#09003d] w-[20%] h-[200vh]  mr-10 mt-10 rounded-xl hidden lg:block'>
         {data.filter((item) => item.title !== props.title).map((item) => (
-
-                    <div className='bg-[#09003d] text-[#fff] p-4 gap-2 rounded-xl w-[100%] grid justify-between border-white border-3 text-center cursor-pointer mb-7 scale-in-bottom hover:bg-[#090111] hover:duration-700 hover:ease-in-out'>
-                      {item.link && (
-                            <Link href={item.link}>
-                        <Image src={item.image} alt={item.title} className='w-full h-44 object-cover rounded-md' width={0} height={0}/>
-                        <h2 className='text-xl font-bold'>{item.title}</h2>
-                        
-                            </Link>
-                        )}
-                    </div>
-                ))}
+            <div key={item.title} className='bg-[#09003d] text-[#fff] p-4 gap-2 rounded-xl w-[100%] grid justify-between border-white border-3 text-center cursor-pointer mb-7 scale-in-bottom hover:bg-[#090111] hover:duration-700 hover:ease-in-out'>
+              {item.link && (
+                    <Link href={item.link}>
+                <Image src={item.image} alt={item.title} className='w-full h-44 object-cover rounded-md' width={0} height={0}/>
+                <h2 className='text-xl font-bold'>{item.title}</h2>
+                
+                    </Link>
+                )}
+            </div>
+        ))}
     </div>
   )
 }
