614a8f15f572461b1cf22380c7100d9b3b413925
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-11 13:00:29 +0500

    about page

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className=' flex flex-col items-center px-5 sm:px-24'>
+        <h1 className='text-2xl sm:text-4xl font-semibold mt-20'>About This Blog</h1>
+        <p className='sm:text-3xl font-normal mt-10'>As a student and a budding developer, I created this blog as part of an exciting assignment, but it’s grown into a personal project where I can share my journey, learning experiences, and the things that fuel my passion. From web development to productivity hacks and more, you’ll find a diverse range of topics that reflect my curiosity and interests.</p>
+        <p className='sm:text-3xl font-normal mt-10'>I believe in continuous learning and growth, and this blog is a testament to that process. I hope that through these posts, you’ll find something that resonates with you—whether it’s a tip that improves your workflow, an idea that sparks your imagination, or just a fun read to brighten your day.</p>
+        <p className='sm:text-3xl font-normal my-10 '>Thank you for visiting, and I look forward to sharing this journey with you.</p>
+    </div>
+  )
+}
+
+export default About
